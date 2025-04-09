ffb2aa1e881417dd6476fed81e0942584393608d
FunB4nan <funb4nan@gmail.com>
2025-04-09 14:19:05 +0300

    bug fix

--- scripts/action.gd
+++ scripts/action.gd
@@ -10,6 +10,7 @@ var target
 var value : int
 
 func act(repeats : int):
+	await get_tree().create_timer(0.01).timeout
 	actionFinished.emit()
 	if repeats == get_parent().get_parent().actionRepeats - 1:
 		queue_free()
